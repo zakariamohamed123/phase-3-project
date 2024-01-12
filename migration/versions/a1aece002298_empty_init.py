ed9e08465c26ff9a814914140d0a385731f49d0a
zakaria <zakimoha2022@gmail.com>
2024-01-12 16:39:26 +0300

    complete

---
+++ migration/versions/a1aece002298_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: a1aece002298
+Revises: 07215d26838c
+Create Date: 2024-01-12 16:35:18.914752
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a1aece002298'
+down_revision = '07215d26838c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
